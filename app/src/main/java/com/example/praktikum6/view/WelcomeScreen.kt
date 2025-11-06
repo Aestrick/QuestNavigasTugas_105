ab338aef697e35beb10279d3bdb5c2df13e590b9
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-11-06 18:53:54 +0700

    Menghapus // <-- SUDAH DISESUAIKAN

--- app/src/main/java/com/example/praktikum6/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/praktikum6/view/WelcomeScreen.kt
@@ -1,4 +1,4 @@
-package com.example.praktikum6.view // <-- SUDAH DISESUAIKAN
+package com.example.praktikum6.view //
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
